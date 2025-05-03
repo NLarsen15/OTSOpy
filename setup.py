c3767b8184fba1de1f539881b1c0e0a44e83c5b9
Nicholas Larsen <nlarsen1505@gmail.com>
2025-05-03 21:40:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='==3.12',
+    python_requires='>=3.12, <3.13',
     install_requires=[
         'psutil==7.0.0',     # Common dependency
     ],
