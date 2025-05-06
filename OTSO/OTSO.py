774b5bba0e8acfdb7d66c2434f7e5269e44a4448
Nicholas Larsen <nlarsen1505@gmail.com>
2025-05-06 22:35:59 +0300

    Update OTSO.py

--- OTSO/OTSO.py
+++ OTSO/OTSO.py
@@ -14,13 +14,9 @@ def Delete():
     setupfile = os.path.join(script_dir, 'setup_complete.txt')
 
     if os.path.exists(file_path):
-        try:
-            os.remove(file_path)
-        except Exception as e:
+        os.remove(file_path)
 
     server_data_folder_path = os.path.join(script_dir, 'Parameters', 'functions', 'ServerData')
     if os.path.exists(server_data_folder_path):
-        try:
-            shutil.rmtree(server_data_folder_path)
-        except Exception as e:
+        shutil.rmtree(server_data_folder_path)
     
