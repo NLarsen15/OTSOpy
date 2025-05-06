13975f74603ab13480fe5899f010fca32d3869ed
Nicholas Larsen <nlarsen1505@gmail.com>
2025-05-06 22:38:09 +0300

    Update OTSO.py

--- OTSO/OTSO.py
+++ OTSO/OTSO.py
@@ -13,8 +13,8 @@ def Delete():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     setupfile = os.path.join(script_dir, 'setup_complete.txt')
 
-    if os.path.exists(file_path):
-        os.remove(file_path)
+    if os.path.exists(setupfile):
+        os.remove(setupfile)
 
     server_data_folder_path = os.path.join(script_dir, 'Parameters', 'functions', 'ServerData')
     if os.path.exists(server_data_folder_path):
