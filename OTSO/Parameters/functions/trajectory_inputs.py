160e3bb8e3373f39f37ecbe37ceb9fcb29c95282
Nicholas Larsen <112173371+NLarsen15@users.noreply.github.com>
2025-10-29 20:37:28 +0900

    Solar Wind Fix

--- OTSO/Parameters/functions/trajectory_inputs.py
+++ OTSO/Parameters/functions/trajectory_inputs.py
@@ -178,7 +178,7 @@ def TrajectoryInputs(Stations,rigidity,customlocations,startaltitude,minaltitude
     if ServerData == 0 and LiveData == 0:
           if vx > 0:
                vx = -1*vx
-          WindCreate = solar_wind.Solar_Wind(vx, vy, vz, bx, by, bz, density, pdyn, Dst, G1, G2, G3, W1, W2, W3, W4, W5, W6, kp, by_avg, bz_avg, n_index, b_index, sym_h_corrected, External)
+          WindCreate = solar_wind.Solar_Wind(vx, vy, vz, bx, by, bz, density, pdyn, Dst, G1, G2, G3, W1, W2, W3, W4, W5, W6, kp, by_avg, bz_avg, n_index, b_index, sym_h_corrected)
           WindArray = WindCreate.GetWind()
 
     Rigidity = rigidity
