e8af390fd58009e7bcb93763db2171263f425c9c
Nicholas Larsen <nlarsen1505@gmail.com>
2026-03-09 16:54:42 +0000

    fixed manifest

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='OTSO',
-    version='1.2.0',
+    version='1.2.2',
     author='Nicholas Larsen',
     author_email='nlarsen1505@gmail.com',
     description='Geomagnetic Cutoff Computation Tool',
